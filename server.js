b876fa123a8cda287f31fd13d905789b989ac11d
Banja David Lotigo <banja@kali.lan>
2023-01-16 05:15:39 -0500

    ADD INDEX FILE - Add express an http client - Add cors a cross platform node package - Add colors to decorate log messages - Listen to the server on the recommended port - Use json for application/json format - Use urlendcode for applicaton/x-www-form-urlencoded format

---
+++ server.js
@@ -0,0 +1,43 @@
+// modules required for the application
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/config");
+const colors = require("colors");
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+
+// parse requests of content type - application/json
+app.use(express.json());
+
+// parse requests of content type - applicaton/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// basic route
+app.get("/", (req, res) => {
+    res.json({ message: "this is a restful applicaton demonstration crud operations"});
+});
+
+// set port, listen for request
+const PORT = process.env.PORT || 8080;
+
+// db configurations
+db.sync()
+    .then((result) => {
+        console.log("Generate Table".green);
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}.`.yellow);
+        });
+         
+    }).catch((err) => {
+        console.log(err.message);
+    });
+
+
+
+
